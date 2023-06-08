d77ab614eacb3cb4dbf93595d0ef93d9bda1bf20
abellmesfin <abellmesfin@gmail.com>
2023-06-07 19:37:55 -0700

    Dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * _strlen_recursion - returns the length of a string.
+ * @s: string length to be measured
+ * Return: int returns the length of a string in _strlen_recursion.
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0') /* if else statement which will add 1*/
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
