2e658b985e3f7a68adc3b238a68841b2e1932830
abellmesfin <abellmesfin@gmail.com>
2023-06-23 18:31:45 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function for printing name
+ * @name: print
+ * @f: function to the pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
