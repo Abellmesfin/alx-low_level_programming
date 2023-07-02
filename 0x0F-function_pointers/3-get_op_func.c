fe396643fc1197684cf247a9061d79afd132f822
abellmesfin <abellmesfin@gmail.com>
2023-07-01 19:10:00 -0700

    3-get_op_func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - function to perform the operation
+ * get_op_func - function that selects function to  perform the operation
  * @s: operator
  *
  * Return: always 0
@@ -19,13 +19,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
 	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
-
 	return (NULL);
 }
